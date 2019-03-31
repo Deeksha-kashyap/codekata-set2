c587fd37c42d7a5e59c491c3f2807ec3687956d4
Deeksha-kashyap <deekshakashyap01@gmail.com>
2019-03-31 19:55:20 +0530

    Update index_element.java

--- index_element.java
+++ index_element.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-public class Position
+public class Program
 {
     public static void main(String[] args) 
     {
         int n;
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter no. of elements you want in array:");
+        System.out.print("Enter the size of array:");
         n = s.nextInt();
         int a[] = new int[n];
-        System.out.println("Enter all the elements:");
+        System.out.println("Enter all the elements of the array:");
         for (int i = 0; i < n; i++) 
         {
             a[i] = s.nextInt();
         }
-        System.out.print("Enter the k th position at which you want to check number:");
+        System.out.print("Enter the k th position:");
         int k = s.nextInt();
         System.out.println("Number:"+a[k-1]);
     }
